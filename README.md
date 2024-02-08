7217b03041f67dcd87a22cbd5d11cde7bacef27f
Andreas Vogler <andreas.vogler@me.com>
2024-02-08 11:40:09 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sparkplugb-printer
 Simple python program printing incoming sparkplugb messages in a human readable format. 
+
+usage: main.py [-h] [-host HOST] [-port PORT] [-topic TOPIC]  
+
+Sparkplug B MQTT client.  
+
+options:  
+  -h, --help    show this help message and exit  
+  -host HOST    the mqtt host to connect to (default: localhost)  
+  -port PORT    the mqtt port to connect to (default: 1883)  
+  -topic TOPIC  the topic to subscribe to (default: spBv1.0/#)  
---
+++ main.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+import sparkplug.sparkplug_b as spb
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Sparkplug B MQTT client.")
+
+# Add the arguments
+parser.add_argument('-host', type=str, help='the mqtt host to connect to', default="localhost")
+parser.add_argument('-port', type=int, help='the mqtt port to connect to', default=1883)
+parser.add_argument('-topic', type=str, help='the topic to subscribe to', default="spBv1.0/#")
+
+
+# Parse the arguments
+args = parser.parse_args()
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("spBv1.0/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("="*80)
+    print("Topic: " + msg.topic)
+    payload = spb.Payload()
+    try:
+        payload.ParseFromString(msg.payload)
+        print(payload)
+    except Exception as e:
+        print("Error decoding: "+str(e)+ " message: "+str(msg.payload))
+
+
+# Create an MQTT client instance
+client = mqtt.Client()
+
+# Assign the on_connect and on_message callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to the MQTT broker
+client.connect(args.host, args.port, 60)
+client.loop_forever()
